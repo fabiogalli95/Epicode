e01acc52b97321c6a7e8d96858f5a612bef5d3ed
fabiogalli95 <38462365+fabiogalli95@users.noreply.github.com>
2024-03-06 16:11:55 +0100

    Esercizio 06 mar

---
+++ Esercizio 06 mar/js/script.js
@@ -0,0 +1,99 @@
+function escapeSpecialCharacters(input) {
+    const replacements = {
+        "'": '&#39;',
+        // Add more replacements if needed
+    };
+
+    // Use a regular expression to match the special characters and replace them
+    return input.replace(/[''']/g, match => replacements[match]);
+}
+
+const createBookCard = (book) => {
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'col-md-3 mb-4';
+
+    const card = `
+        <div class="card">
+            <img src="${book.img}" class="card-img-top" alt="${book.title}">
+            <div class="card-body">
+                <h5 class="card-title">${book.title}</h5>
+                <p class="card-text">Prezzo: ${book.price}$</p>
+                <button class="btn btn-danger" onclick="removeCard(this)">Scarta</button>
+                <button class="btn btn-success" onclick="addToCart('${book.title}', ${book.price})">Compra ora</button>
+            </div>
+        </div>
+    `;
+    cardDiv.innerHTML = card;
+
+    return cardDiv;
+}
+ 
+const removeCard = (element) => {
+    console.log(element);
+    const card = element.closest('.col-md-3');
+    card.remove();
+};
+ 
+const addToCart = (title, price) => {
+    console.log(title)
+    const cartList = document.getElementById('cartList');
+    const listItem = document.createElement('li');
+    listItem.className = 'list-group-item';
+    listItem.innerHTML = `${title} - ${price}$ <button class="btn btn-danger btn-sm float-end" onclick="removeFromCart(this)">Rimuovi</button>`;
+    cartList.appendChild(listItem);
+
+   
+    updateCartStorage();
+};
+
+
+const removeFromCart = (element) => {
+    const listItem = element.closest('li');
+    listItem.remove();
+
+
+    updateCartStorage();
+};
+
+ const updateCartStorage = () => {
+    const cartList = document.getElementById('cartList');
+    const cartItems = cartList.innerHTML;
+    localStorage.setItem('cartItems', cartItems);
+}
+
+
+const checkoutBtn = document.getElementById('checkoutBtn');
+        checkoutBtn.addEventListener('click', function () {
+            alert('Grazie per il tuo acquisto!');
+           
+        });
+
+window.onload = function(){
+    init();
+}
+
+const init = () => {
+       let response = fetch('https://striveschool-api.herokuapp.com/books')
+            .then(response => response.json())
+            .then(data => {
+                const bookListContainer = document.getElementById('bookList');
+                data.forEach(book => {
+                    const card = createBookCard(book);
+                    bookListContainer.appendChild(card);
+                });
+                return data;
+            })
+            .catch(error => console.error('Errore nella richiesta:', error));
+            
+        
+     
+
+        const storedCartItems = localStorage.getItem('cartItems');
+        if (storedCartItems) {
+            const cartList = document.getElementById('cartList');
+            cartList.innerHTML = storedCartItems;
+        }
+
+      
+        
+    };
